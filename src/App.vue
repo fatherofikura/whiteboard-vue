c1430ba26a8e69c39d4afa84ad0f4d6394b4a498
fatherofikura <fatherofikura0107@gmail.com>
2018-10-29 07:52:24 +0900

    menubar追加(中)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <whiteboard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Whiteboard from './components/Whiteboard'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Whiteboard
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Whiteboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="whiteboard">
+    <menubar />
+  </div>
+</template>
+
+<script>
+import Menubar from './Menubar'
+export default {
+  name: 'whiteboard',
+  components: {
+    Menubar,
+  }
+}
+</script>
